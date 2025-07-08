fb86472991e210b7ef202d3c8eb402ba95602d25
ChrisChoi314 <mychoi314@gmail.com>
2025-07-07 21:02:19 -0400

    changed some stuff back to html

--- assets/js/shortcut-key.js
+++ assets/js/shortcut-key.js
@@ -1,4 +1,3 @@
-
 // Check if the user is on a Mac and update the shortcut key for search accordingly
 document.addEventListener("readystatechange", () => {
   if (document.readyState === "interactive") {
