0c83a66b8272a82c58ec23a3b28087f007b998cf
ChrisChoi314 <mychoi314@gmail.com>
2025-07-14 22:41:39 +0000

    Deploying to gh-pages from @ ChrisChoi314/chrischoi314.github.io@0778fd0bba7d3894e900f83dbca2802eafe416a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -47,7 +47,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-fto-kinchranks",
+        },{id: "post-rebuttal-to-tingman-39-s-video",
+      title: "Rebuttal to Tingman&#39;s Video",
+      description: "religious apologetics rebuttal",
+      section: "Posts",
+      handler: () => {
+        window.location.href = "/blog/2025/rebuttal-tingman/";
+      },
+    },{id: "post-fto-kinchranks",
       title: "FTO Kinchranks",
       description: "rankings for FTOers for all time formats",
       section: "Posts",
